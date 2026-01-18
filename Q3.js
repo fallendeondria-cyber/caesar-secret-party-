35c79c5797842f3529b24bfa5505d4c5ed2ce56b
fallendeondria-cyber <fallendeondria@gmail.com>
2026-01-17 20:31:03 -0500

    Create Q3.js

---
+++ Q3.js
@@ -0,0 +1,5 @@
+let partyLocation = "GARDEN"; // This could change
+const shiftValue = 3; // This stays constant
+let encryptedMessage = ""; // Will change after encryption
+let decryptedMessage = ""; // Will change after decryption
+let isPartySafe = false; // Can change based on situation
